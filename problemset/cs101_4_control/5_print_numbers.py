585dd97eeee096f18e6da4580fa12bd2b813df38
lastland <locallab-seoul@gmail.com>
2018-03-28 18:19:51 +0900

    add problemset 4

---
+++ problemset/cs101_4_control/5_print_numbers.py
@@ -0,0 +1,17 @@
+# Define a procedure, print_numbers, that takes
+# as input a positive whole number, and prints
+# out all the whole numbers from 1 to the input
+# number.
+
+# Make sure your procedure prints "upwards", so
+# from 1 up to the input number.
+
+
+
+
+
+
+#print(numbers(3))
+#>>> 1
+#>>> 2
+#>>> 3
