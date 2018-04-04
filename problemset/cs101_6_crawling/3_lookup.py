3c319681c21e72976451a588e11a149f6c138e79
lastland <locallab-seoul@gmail.com>
2018-04-04 13:30:13 +0900

    Add problemset 6

---
+++ problemset/cs101_6_crawling/3_lookup.py
@@ -0,0 +1,26 @@
+# Define a procedure, lookup,
+# that takes two inputs:
+
+# - an index
+# - keyword
+
+# The procedure should return a list
+# of the urls associated
+# with the keyword. If the keyword
+# is not in the index, the procedure
+# should return an empty list.
+
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+
+def lookup(index,keyword):
+    pass
+
+
+
+
+
+
+# print(lookup(index,'udacity'))
+# >>> ['http://udacity.com','http://npr.org']
