a97ae14e62f5248d889bd21925894a26933a7bdf
lastland <locallab-seoul@gmail.com>
2018-05-16 13:54:32 +0900

    Add problemset 1

---
+++ problemset/cs101_1_hello/2_speed_of_light.py
@@ -0,0 +1,3 @@
+# speed_of_light = 299792458   meters per second
+# centimeters = 100            one meter is 100 centimeters
+# nanosecond = 1.0/1000000000  one billionth of a second
---
+++ problemset/cs101_1_hello/3_variables.py
@@ -0,0 +1,5 @@
+# speed_of_light in meters per second
+# cycles_per_second is 2.7 GHz
+
+speed_of_light = 299792458.0
+cycles_per_second = 2700000000.0
