d68f00330f4d51ac5bf565dae03ba25dbf9ae682
lastland <locallab-seoul@gmail.com>
2018-05-23 14:32:34 +0900

    Add problemset 3

---
+++ problemset/cs101_3_procedure/1_square.py
@@ -0,0 +1,14 @@
+# Define a procedure, square, that takes one number
+# as its input, and returns the square of that
+# number (result of multiplying
+# the number by itself).
+# To help you out, the code for sum(a,b) is below.
+
+
+def sum(a, b):
+    c = a + b
+    return c
+
+
+# print(square(5))
+# >>> 25
