f668f3fac429ad8ca07cfadf45ddf84eac6a6233
lastland <locallab-seoul@gmail.com>
2018-05-23 14:32:39 +0900

    Add problemset 4

---
+++ problemset/cs101_4_control/8_print_all_links.py
@@ -0,0 +1,34 @@
+def get_page(url):
+    try:
+        import urllib.request
+        return urllib.request.urlopen(url).read().decode("utf8")
+    except:
+        return ""
+
+
+def get_next_target(page):
+    start_link = page.find('<a href=')
+
+    #Insert your code below here
+    if start_link == -1:
+        return None, 0
+
+    start_quote = page.find('"', start_link)
+    end_quote = page.find('"', start_quote + 1)
+    url = page[start_quote + 1:end_quote]
+    return url, end_quote
+
+
+def print_all_links(page):
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            print(url)
+            page = page[endpos:]
+        else:
+            break
+
+
+content = get_page('http://naver.com')
+
+print_all_links(content)
